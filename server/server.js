2faf194dbdde86c60b34163e2a46f5d1e5c03567
joaochioda <joao.chioda@dextra-sw.com>
2022-07-28 22:13:01 -0300

    wqip

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-server.listen(3333, () => {
+app.listen(3333, () => {
   console.log("listening on :3333");
 });
